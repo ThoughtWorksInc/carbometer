adbef381f1c169e915aa73dd1b16bc381a08fbda
Rob Pak <rob@carbonfive.com>
2011-01-13 17:53:01 -0800

    initial impound

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carbometer::Application.config.secret_token = '9c9eff9f42ae85677530e9a59611e06c03962bbca71ca76a2af4e922c6c9a0b44388b5b3325f4a72be5ee565c9c05e92632ed06422040cd60e112ae322e47c08'
