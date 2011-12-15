1ad57a90c34cdbfd906cfc746a1fb62dda79188e
Jonah Williams <jonah@carbonfive.com>
2011-12-14 18:39:11 -0800

    only check for existing users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def create
-    auth = request.env['rack.auth']
+    auth = request.env['omniauth.auth']
     unless @auth = Authentication.find_from_hash(auth)
       # Create a new user or add an auth to existing user, depending on
       # whether there is already a user signed in.
-      @auth = Authentication.create_from_hash(auth, current_user)
+      # @auth = Authentication.create_from_hash(auth, current_user)
     end
     # Log the authorizing user in.
     self.current_user = @auth.user
