d503cd7147bb1c5d1c91b58e10eea0f047ba34cd
Sean Durham <sean@sdurham.net>
2011-12-14 16:49:25 -0800

    Show static list of projects.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,36 @@ class DashboardsController < ApplicationController
   # GET /dashboards
   # GET /dashboards.json
   def index
+    @projects = [
+      {
+        :name => "Blockbridge",
+        :team => [ "Ingar", "Christian", "Jim", "Sean", "Elias" ]
+      },
+      {
+        :name => "Consulting MD",
+        :team => [ "Dave H.", "Suzanna S.", "Courtney", "Mike P.", "Steven H." ]
+      },
+      {
+        :name => "JobYou",
+        :team => [ "Jon C." ]
+      },
+      {
+        :name => "Booty Log",
+        :team => [ "August", "Jonah", "Rudy", "Rit", "Eric I." ]
+      },
+      {
+        :name => "Pocket Gems",
+        :team => [ "Clark", "Winston" ]
+      },
+      {
+          :name => "C5 Website",
+          :team => [ "Kasha" ]
+      },
+      {
+        :name => "Timesheet",
+        :team => [ "James" ]
+      }
+    ]
 
   end
 end
