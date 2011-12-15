da244f6b737950a48030e6ac124858ea642a843c
Jonah Williams <jonah@carbonfive.com>
2011-12-14 18:38:46 -0800

    route authentication failures

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Carbometer::Application.routes.draw do
   resources :authentications, :only => [:index, :destroy]
+  match 'auth/failure', :to =>'authentications#index'
   match '/auth/:provider/callback', :to => 'sessions#create'
   
   root :to => 'dashboards#index'
