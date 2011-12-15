630667c3e26f1eb00820d4de8341872490289312
Jonah Williams <jonah@carbonfive.com>
2011-12-14 19:01:32 -0800

    try to resolve type error querying for authentication uid as an int

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -4,11 +4,11 @@ class Authentication < ActiveRecord::Base
   validates_uniqueness_of :uid, :scope => :provider
   
   def self.find_from_hash(hash)
-    find_by_provider_and_uid(hash['provider'], hash['uid'])
+    find_by_provider_and_uid(hash['provider'], hash['uid'].to_s)
   end
 
   def self.create_from_hash(hash, user = nil)
     user ||= User.create_from_hash!(hash)
-    Authentication.create(:user => user, :uid => hash['uid'], :provider => hash['provider'])
+    Authentication.create(:user => user, :uid => hash['uid'].to_s, :provider => hash['provider'])
   end
 end
