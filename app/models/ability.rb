d5356d838622ea54d0375f43f0f5acfb5d211481
Jonah Williams <jonah@carbonfive.com>
2011-12-15 10:25:20 -0800

    all users can manage the dashboard

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :index, DashboardsController
+    can :manage, DashboardsController
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
