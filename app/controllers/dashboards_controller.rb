87c1515f6bde03145c0fb5560fb113a0acf40e01
Sean Durham <sean@sdurham.net>
2011-12-14 17:44:54 -0800

    Add gravatars to team members.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,35 +2,52 @@ class DashboardsController < ApplicationController
   # GET /dashboards
   # GET /dashboards.json
   def index
+    @people = {
+      :sean => {
+        :name => "Sean D.",
+        :email => "sean@sdurham.net"
+      },
+      :winston => {
+        name: "Winston W.",
+        email: "winston@carbonfive.com"
+      }
+    }
+
+    def team_members(members)
+      members.map { |m| @people[m] }
+    end
+
     @projects = [
       {
         :name => "Blockbridge",
-        :team => [ "Ingar", "Christian", "Jim", "Sean", "Elias" ]
-      },
-      {
-        :name => "Consulting MD",
-        :team => [ "Dave H.", "Suzanna S.", "Courtney", "Mike P.", "Steven H." ]
-      },
-      {
-        :name => "JobYou",
-        :team => [ "Jon C." ]
-      },
-      {
-        :name => "Booty Log",
-        :team => [ "August", "Jonah", "Rudy", "Rit", "Eric I." ]
-      },
-      {
-        :name => "Pocket Gems",
-        :team => [ "Clark", "Winston" ]
-      },
-      {
-          :name => "C5 Website",
-          :team => [ "Kasha" ]
-      },
-      {
-        :name => "Timesheet",
-        :team => [ "James" ]
+        :image => "blockbridge.png",
+        :team => team_members([:winston, :sean])
       }
+      #{
+      #  :name => "Consulting MD",
+      #  :team => [ "Dave H.", "Suzanna S.", "Courtney", "Mike P.", "Steven H." ]
+      #},
+      #{
+      #  :name => "JobYou",
+      #  :team => [ "Jon C." ]
+      #},
+      #{
+      #  :name => "Booty Log",
+      #  :team => [ "August", "Jonah", "Rudy", "Rit", "Eric I." ]
+      #},
+      #{
+      #  :name => "Pocket Gems",
+      #  :image => "PocketGems.png",
+      #  :team => [ "Clark", "Winston" ]
+      #},
+      #{
+      #    :name => "C5 Website",
+      #    :team => [ "Kasha" ]
+      #},
+      #{
+      #  :name => "Timesheet",
+      #  :team => [ "James" ]
+      #}
     ]
 
   end
--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,8 @@
+require 'digest/md5'
+
 module DashboardsHelper
+
+  def gravatar (email)
+    "http://www.gravatar.com/avatar/#{ Digest::MD5.hexdigest(email.strip.downcase) }"
+  end
 end
