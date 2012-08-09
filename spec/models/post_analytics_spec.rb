3def9854d62320266f77a228629abc3f12d8056e
Rob Pak <rob@carbonfive.com>
2012-08-09 13:42:07 -0700

    Move Webmocks to Spec Helper

--- spec/models/post_analytics_spec.rb
+++ spec/models/post_analytics_spec.rb
@@ -26,18 +26,7 @@ describe PostAnalytics do
 
   describe '::find_all_by_date_range' do
     before do
-      stub_request(:post, "https://accounts.google.com/o/oauth2/token").
-        to_return(status: 200,
-                  body: fixture_for('oauth'))
-
-      stub_request(:get, "https://www.googleapis.com/discovery/v1/apis/analytics/v3/rest").
-        to_return(status: 200,
-                  body: fixture_for('discovery'))
-
-      stub_request(:get, /www\.googleapis\.com\/analytics\/v3\/data\/ga/).
-        to_return(status: 200,
-                  body: fixture_for('google_analytics'),
-                  headers: {'Content-Type' => 'application/json'})
+      stub_requests_for(:google)
 
       @page_view_data = PostAnalytics.find_all_by_date_range(Date.today, Date.today)
     end
--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,1 +1,20 @@
 require 'webmock/rspec'
+
+def stub_requests_for(requests_for)
+  self.send("stub_requests_for_#{requests_for}")
+end
+
+def stub_requests_for_google
+  stub_request(:post, "https://accounts.google.com/o/oauth2/token").
+    to_return(status: 200,
+              body: fixture_for('oauth'))
+
+  stub_request(:get, "https://www.googleapis.com/discovery/v1/apis/analytics/v3/rest").
+    to_return(status: 200,
+              body: fixture_for('discovery'))
+
+  stub_request(:get, /www\.googleapis\.com\/analytics\/v3\/data\/ga/).
+    to_return(status: 200,
+              body: fixture_for('google_analytics'),
+              headers: {'Content-Type' => 'application/json'})
+end
