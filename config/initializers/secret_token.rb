4e23fe5b4f131bee5c48b182f5385692333a514f
Rob Pak <rob@carbonfive.com>
2012-08-09 09:57:36 -0700

    Externalize Secret Token as Env Variables

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Carbometer::Application.config.secret_token = '9a7d596ca7b2e970ed590f90c6340701ba2ecae7b201cfb745753dd9b58ab260297254cbb9afac6d3544a2f640dc56b7185d32fc08d9786148214cfb145e8790'
+Carbometer::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
