af03097ca31bdef0452ac9b4035aa87f3c86b409
Rob Pak <rob@carbonfive.com>
2012-08-10 15:54:37 -0700

    Find Most Popular Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,21 @@
 class Post < ActiveRecord::Base
+
   attr_accessible :title,
                   :path
 
   has_many        :statistics, dependent: :destroy
+
+  def cumulative_visit_count
+    statistics.sum(:visit_count)
+  end
+
+  def self.popular
+    Post.find(:all,
+              joins: :statistics,
+              select: 'posts.*, SUM(statistics.visit_count) as visit_sum',
+              group: 'posts.id, posts.title, posts.path',
+              limit: 10,
+              order: 'visit_sum desc')
+  end
+
 end
